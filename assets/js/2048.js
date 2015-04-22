90f554af1a9a19560b4b6397e7407d0f72fbb662
Rizki Sunaryo <rizkisunaryo@gmail.com>
2015-04-22 22:29:02 +0800

    add function to paint tiles

--- assets/js/2048.js
+++ assets/js/2048.js
@@ -1,4 +1,14 @@
-$(function() {      
+var numbers=[];
+var column=[];
+var ROW_COUNT = 4;
+var COLUMN_COUNT = 4;
+
+$(function() { 
+  numbers[0] = column;
+  numbers[0][0] = 8;
+  paintNumbers(numbers,ROW_COUNT,COLUMN_COUNT);
+  
+
   //Enable swiping...
   $("#bodyContainer").swipe( {
     //Generic swipe handler for all directions
@@ -9,3 +19,15 @@ $(function() {
     threshold:0
   });
 });
+
+function paintNumbers(pNumbers,pRowCount,pColumnCount) {
+  for (var i1=0; i1<=pRowCount-1; i1++) {
+    for (var i2=0; i2<=pColumnCount-1; i2++) {
+      if (typeof pNumbers[i1] !== 'undefined' && typeof pNumbers[i1][i2] !== 'undefined' && pNumbers[i1][i2]!=null) {
+        var html='<div class="tile"><div>'+pNumbers[i1][i2]+'</div></div>';
+        var selector = '#cell'+i1+''+i2;
+        $(selector).html(html);
+      }
+    }
+  }
+}
