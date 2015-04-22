2bc4dedf55a83f538170c78a49f1a8a91ac013ec
Rizki Sunaryo <rizkisunaryo@gmail.com>
2015-04-22 22:50:30 +0800

    Add tile randomly

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Development steps:  
 1. Create design 4x4  
-2. Move tiles, and add 2 tiles with the same number  
+2. Add tile randomly
+3. Move tiles, and add 2 tiles with the same number  
 a. Test swipe gesture  
 b. Move tiles  
 c. Add 2 tiles with the same number  
-3. Add score  
-4. Add tile randomly
+4. Add score  
--- assets/js/2048.js
+++ assets/js/2048.js
@@ -1,11 +1,14 @@
 var numbers=[];
-var column=[];
 var ROW_COUNT = 4;
 var COLUMN_COUNT = 4;
 
 $(function() { 
-  numbers[0] = column;
-  numbers[0][0] = 8;
+  for (var i=0; i<=ROW_COUNT-1; i++) {
+    var column=[];
+    numbers[i]=column;
+  };
+  // numbers[0][0] = 8;
+  addRandomTile(numbers,ROW_COUNT,COLUMN_COUNT);
   paintNumbers(numbers,ROW_COUNT,COLUMN_COUNT);
   
 
@@ -20,12 +23,25 @@ $(function() {
   });
 });
 
+function addRandomTile(pNumbers,pRowCount,pColumnCount) {
+  var row = Math.floor((Math.random() * (pRowCount-1)) + 0.5);
+  var col=Math.floor((Math.random() * (pColumnCount-1)) + 0.5);
+  if (typeof pNumbers[row] !== 'undefined' && typeof pNumbers[row][col] !== 'undefined' && pNumbers[row][col]!=null) {
+    addRandomTile(pNumbers,pRowCount,pColumnCount);
+  }
+  else {
+    console.log(row+":"+col);
+    pNumbers[row][col]=2;
+  }
+}
+
 function paintNumbers(pNumbers,pRowCount,pColumnCount) {
   for (var i1=0; i1<=pRowCount-1; i1++) {
     for (var i2=0; i2<=pColumnCount-1; i2++) {
       if (typeof pNumbers[i1] !== 'undefined' && typeof pNumbers[i1][i2] !== 'undefined' && pNumbers[i1][i2]!=null) {
         var html='<div class="tile"><div>'+pNumbers[i1][i2]+'</div></div>';
         var selector = '#cell'+i1+''+i2;
+        console.log(selector);
         $(selector).html(html);
       }
     }
